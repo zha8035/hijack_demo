f6bbfdf3ebfeecf3482fc2c3042bef27333f289e
elephant <xiangwenpin@yunyao-inc.com>
2013-11-16 11:36:07 +0800

    新建工程

---
+++ hijack_demo/hijack_demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  hijack_demo
+//
+//  Created by demo on 13-11-16.
+//  Copyright (c) 2013年 Junsheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ hijack_demo/hijack_demoTests/hijack_demoTests.m
@@ -0,0 +1,34 @@
+//
+//  hijack_demoTests.m
+//  hijack_demoTests
+//
+//  Created by demo on 13-11-16.
+//  Copyright (c) 2013年 Junsheng. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface hijack_demoTests : XCTestCase
+
+@end
+
+@implementation hijack_demoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
